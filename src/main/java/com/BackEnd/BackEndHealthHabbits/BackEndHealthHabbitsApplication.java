72911179ee3e2bf8da4f7b36026a9877fb69e4a2
peri-carvalho <pericles.carvalho@precojusto.app.br>
2025-04-22 14:26:49 -0400

    first commit

---
+++ src/main/java/com/BackEnd/BackEndHealthHabbits/BackEndHealthHabbitsApplication.java
@@ -0,0 +1,13 @@
+package com.BackEnd.BackEndHealthHabbits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndHealthHabbitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndHealthHabbitsApplication.class, args);
+	}
+
+}
